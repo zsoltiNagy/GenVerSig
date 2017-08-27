506f504bb693876e23852f2322fdbc8e2cc8ded6
Zsolt Nagy <zsoltiusz.nagy@gmail.com>
2017-08-27 16:41:26 +0200

    Add files via upload

---
+++ java_keys/src/GenSig.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.security.*;
+public class GenSig {
+    public static void main(String[] args) {
+        /* Generate DSA signature */
+        if (args.length != 1) {
+            System.out.println("Usage: GenSig nameOfFileToSign");
+        }
+        else try {
+
+            // the rest of the code goes here
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("DSA", "SUN");
+            SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
+            keyGen.initialize(1024, random);
+            KeyPair pair = keyGen.generateKeyPair();
+            PrivateKey priv = pair.getPrivate();
+            PublicKey pub = pair.getPublic();
+            Signature dsa = Signature.getInstance("SHA1withDSA", "SUN");
+            dsa.initSign(priv);
+            FileInputStream fis = new FileInputStream(args[0]);
+            BufferedInputStream bufin = new BufferedInputStream(fis);
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = bufin.read(buffer)) >= 0) {
+                dsa.update(buffer, 0, len);
+            }
+            bufin.close();
+            byte[] realSig = dsa.sign();
+            /* save the signature in a file */
+            FileOutputStream sigfos = new FileOutputStream("sig");
+            sigfos.write(realSig);
+            sigfos.close();
+            /* save the public key in a file */
+            byte[] key = pub.getEncoded();
+            FileOutputStream keyfos = new FileOutputStream("suepk");
+            keyfos.write(key);
+            keyfos.close();
+
+
+
+
+        } catch (Exception e) {
+            System.err.println("Caught exception " + e.toString());
+        }
+    }
+}
